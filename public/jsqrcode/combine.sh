67da6d35729f2e19a1af9d364dea67a8059c6edc
Daniel Weigl <DanielWeigl@gmx.at>
2016-09-01 10:55:58 +0200

    Pages build for cc8e278cfb0a627868d4372b8fab804d510f88bd

---
+++ public/jsqrcode/combine.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat grid.js version.js detector.js formatinf.js errorlevel.js bitmat.js datablock.js bmparser.js datamask.js rsdecoder.js gf256poly.js gf256.js decoder.js qrcode.js findpat.js alignpat.js databr.js > qr.js
